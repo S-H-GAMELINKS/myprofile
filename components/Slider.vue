aea77fe42068e840bc99bcca20e9da9b96f656aa
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-13 00:05:38 +0900

    Create Slider.vue components & add game's image

---
+++ components/Slider.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-carousel :interval="4000" type="card" width="600">
+    <el-carousel-item v-for="(s, key, index) in slides" :key="index">
+      <img :src="s.path" width="300" height="200" />
+      {{s.content}}
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
+
+<script>
+  export default {
+    data: function() {
+        return {
+            slides: this.$parent.slides,
+            log: console.log(this.$parent.slides)
+        }
+    }
+  }
+</script>
--- pages/game.vue
+++ pages/game.vue
@@ -5,11 +5,12 @@
   </div>
   <section class="container">
     <div>
+      <slider :slides="slides"></slider>
       <h1 class="title">
         Game
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        これまでに制作してきたゲーム
       </h2>
     </div>
   </section>
@@ -18,10 +19,34 @@
 
 <script>
 import NavMenu from '~/components/NavMenu.vue'
+import Slider from '~/components/Slider.vue'
 
 export default {
+  data: function() {
+    return {
+      slides: [
+        {path: "/game/links1.png", content: "開発したノベルゲームエンジン"},
+        {path: "/game/links2.png", content: "半角英数のタグを使用します"},
+        {path: "/game/hanakotoba1.png", content: "花言葉を使った短編"},
+        {path: "/game/hanakotoba2.png", content: "コーディングなどを担当しました"},
+        {path: "/game/igv1.png", content: "異能コメディ"},
+        {path: "/game/igv2.png", content: "コーディングや演出を担当"},
+        {path: "/game/bml1.png", content: "クトゥルフ神話を使った短編"},
+        {path: "/game/bml2.png", content: "コーディングなどほぼ全てを担当"},
+        {path: "/game/ve1.png", content: "ダメ人間たちのオーケストラ"},
+        {path: "/game/ve2.png", content: "原案などを担当しました"},
+        {path: "/game/vendetta1.png", content: "出雲を舞台に青年と少女がふれあう"},
+        {path: "/game/vendetta2.png", content: "シナリオなどを担当しました"},
+        {path: "/game/vendetta1.png", content: "復讐に取りつかれた男の短編"},
+        {path: "/game/vendetta2.png", content: "シナリオなどを全て担当"},
+        {path: "/game/TOL1.png", content: "家族を失った男性と少女のふれあい"},
+        {path: "/game/TOL2.png", content: "こちらもシナリオ他全てを担当"},
+      ]
+    }
+  },
   components: {
     NavMenu,
+    Slider
   }
 }
 </script>
