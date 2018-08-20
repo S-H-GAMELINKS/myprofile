e5454746c002d82c6e27a7874dbf1266b8c9f20f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-20 14:58:59 +0900

    fix Slider.vue

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,11 +1,13 @@
 <template>
-  <v-carousel>
-    <v-carousel-item
-      v-for="(item,i) in slides"
-      :key="i"
-      :src="item.path"
-      {{item.content}}
-    ></v-carousel-item>
+  <v-carousel hide-delimiters=true>
+    <v-carousel-item v-for="(item,i) in slides" :key="i">
+      <v-card>
+        <v-card-media :src="item.path" height="440px"/>
+          <v-card-text>
+            {{item.content}}
+          </v-card-text>
+      </v-card>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
